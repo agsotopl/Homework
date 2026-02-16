f5df0be3d5b34a9793a0cc38d3e9abd59457d1e0
Andres Soto Plaza <119691164+agsotopl@users.noreply.github.com>
2026-02-16 04:11:41 +0000

    commit 1 hw 4

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,10 +9,14 @@ st.set_page_config(
 # Define each HW page
 hw1 = st.Page("HW/hw-1.py", title="HW 1")
 hw2 = st.Page("HW/hw-2.py", title="HW 2")
-hw3 = st.Page("HW/hw-3.py", title="HW 3", default=True)
+hw3 = st.Page("HW/hw-3.py", title="HW 3")
+hw4 = st.Page("HW/hw-4.py", title="HW 4", default=True)
 
 # Build navigation
-pg = st.navigation([hw1, hw2, hw3])
+pg = st.navigation([hw1, hw2, hw3, hw4])
 
 # Run selected page
 pg.run()
+
+
+
